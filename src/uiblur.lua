90fabfa531cbbb2969b8835f3f42d68c58227673
esore <88977097+IcantAffordSynapse@users.noreply.github.com>
2026-08-19 20:55:07 +1000

    Update uiblur.lua

--- src/uiblur.lua
+++ src/uiblur.lua
@@ -5,7 +5,7 @@ local blurr = {}
 function blurr:blur(element)
     local RunService = game:GetService('RunService')
     local camera = workspace.CurrentCamera
-    local MTREL = Enum.Material.Neon
+    local MTREL = "Glass"
     local binds = {}
     local root = Instance.new('Folder', camera)
     root.Name = 'BlurSnox'
